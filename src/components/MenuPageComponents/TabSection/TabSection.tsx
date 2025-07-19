3364639deaf964d81b15562cbad62f6eb269f067
Mohamed-Hashim-km <hashimhx3@gmail.com>
2025-07-19 07:34:49 +0530

    saladCondition

--- src/components/MenuPageComponents/TabSection/TabSection.tsx
+++ src/components/MenuPageComponents/TabSection/TabSection.tsx
@@ -16,17 +16,20 @@ const TabSection: React.FC<Props> = ({ menus, onMenuClick, selectedMenuId }) => 
       }}
     >
       <div className="flex space-x-4">
-        {menus.map((menu) => (
-          <button
-            key={menu._id}
-            onClick={() => onMenuClick(menu)}
-            className={`px-6 py-2 border border-[#C5A059] text-[12px] lg:text-base text-white font-bold ${
-              menu._id === selectedMenuId ? "bg-[#C5A059]" : "bg-black"
-            }`}
-          >
-            {menu.name.toUpperCase()}
-          </button>
-        ))}
+        {menus.map((menu) => {
+          if (menu.name.toLowerCase().includes("salads")) return null; 
+          return (
+            <button
+              key={menu._id}
+              onClick={() => onMenuClick(menu)}
+              className={`px-6 py-2 border border-[#C5A059] text-[12px] lg:text-base text-white font-bold ${
+                menu._id === selectedMenuId ? "bg-[#C5A059]" : "bg-black"
+              }`}
+            >
+              {menu.name.toUpperCase()}
+            </button>
+          );
+        })}
       </div>
     </div>
   );
